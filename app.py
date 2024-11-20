c6e82ca378675dbc4ca1ffde0deac48ad807e6c7
russokun <agustin.russo2323@gmail.com>
2024-11-20 14:51:15 -0300

    commit para instalacion de dependencias.

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+url="https://midas.minsal.cl/farmacia_v2/WS/getLocales.php"
+
+response = requests.get(url)
+
+st.set_page_config(page_title="Farmacias Chile", page_icon=":shark:", layout="wide")
+
+#intro
+
+with st.container():
+    st.header('Datos Oficiales')
+    st.title('Farmacias Chile')
+    st.write('Esta aplicación web permite visualizar la ubicación de las farmacias en Chile, además de información relevante sobre ellas.')
