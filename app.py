e8ba6b92c65150dab56855788d11f04fb4ecb953
russokun <agustin.russo2323@gmail.com>
2024-11-20 15:23:56 -0300

    commit para visualizar la pagina con streamlit.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ url="https://midas.minsal.cl/farmacia_v2/WS/getLocales.php"
 
 response = requests.get(url)
 
+data=response.json()
+
 st.set_page_config(page_title="Farmacias Chile", page_icon=":shark:", layout="wide")
 
 #intro
@@ -14,3 +16,4 @@ with st.container():
     st.header('Datos Oficiales')
     st.title('Farmacias Chile')
     st.write('Esta aplicación web permite visualizar la ubicación de las farmacias en Chile, además de información relevante sobre ellas.')
+    
